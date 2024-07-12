d8a14334df568142b3c4c184aff8a0f435dc9f32
Chris K <mangoman48@gmail.com>
2024-07-12 17:09:22 -0400

    Update proj_1_code.py

---
+++ Project 1/proj_1_code.py
@@ -0,0 +1,20 @@
+,,,
+Chris Kayembe 
+Project 1: Website Login
+,,,
+
+# Password and User Id requirements 
+
+
+  # Test 1: Initializing the variables
+
+
+
+
+
+  # Test 2: List to handle error messages:
+
+
+
+
+  # Test 3: 
