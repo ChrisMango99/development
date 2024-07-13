ebda0471cf96c2222e1af0487376798264e415fe
Chris Kayembe <chris.kayembe@talent.cancode.org>
2024-07-13 15:18:44 -0400

    Inital Push

--- Project 1/proj_1_code.py
+++ Project 1/proj_1_code.py
@@ -1,24 +1,61 @@
-,,,
-Chris Kayembe 
-Project 1: Website Login
-,,,
+''' Name: Chris Kayembe 
+    Project 1: Website Sign-Up 
+    
+Project Description: Website Sign-Up
+In this project, you will write a program that prompts a user to sign up for a website. The user must come up with a username and password, then log in using the username and password.
+There are requirements for both the username and password. If either one doesn't meet the requirements, print an appropriate error message and ask the username for a new username and password.
+If they are both valid, store them as variables. Then ask the user to log in using the username and password they chose.
+    
+''' 
 
-# Password and User Id requirements 
+'''
+Username requirements: (Must start with a lowercase letter and only contain letters, numbers and underscores.)
+
+Taken Usernames: 'admin', 'admin123', 'user1', 'superuser'
+
+Password requirements: (Must be atleast 8 characters long, Contains at least one Uppercase and lowercase letter, Contain at least one digit,
+Contains at least one of the characters: !,?,@,#,^,&,*,_,-. Also it must not have any spaces.)
+  
+'''
+#Step 1: print statement clearly explaining to the user both the username and password requirements.
+
+Rules = input('Welcome User, please enter a username and password to signup ')
+print(Rules)
 
 
-  # Test 1: Initializing the variables
 
 
 
 
 
-  # Test 2: List to handle error messages:
+
+#Step 2
 
 
 
 
-  # Test 3: Starting While loop
+
+#Step 3
 
 
 
-  # Test 4: 
+
+#Step 4
+
+
+
+
+#Step 5
+
+
+
+
+
+
+#Step 6
+
+
+
+
+
+
