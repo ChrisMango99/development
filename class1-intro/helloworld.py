aa32e4cdbc05e9246e9e0aa8ac0cb4970c0924e6
Chris Kayembe <chris.kayembe@talent.cancode.org>
2024-07-13 14:23:20 -0400

    final commit

--- class1-intro/helloworld.py
+++ class1-intro/helloworld.py
@@ -1,1 +1,16 @@
-print("Hello World")
+print("Hello World") # This is the most basic code for python and any other programming langauge 
+
+
+'''Git Hub is the workspace we will be using for this class. 
+
+
+Here are some of the most important commands for git hub.
+
+git add . - (First Command we type in the console)
+git commit -m (We write a message of the commit, ex. inital push)
+git push (command we type after the message)
+git pull (pull from the github repository)
+
+That's pretty much all there is to github. Think of it like google drive. 
+
+'''
---
+++ class10-range/ranges (2).py
@@ -0,0 +1,81 @@
+'''
+Ranges
+    - Must be integers, whole numbers
+    - Used to iterate
+    - Does not store all the numbers, but knows what they should be
+    - Simple to write and create
+    -saves memory
+'''
+
+# Using the range function, lets count to 20
+
+
+
+
+''' Let's try these '''
+
+
+
+
+
+
+
+'''
+Write a range that is every five years from 1960 to 2000.
+'''
+
+
+'''
+range(start, stop, step)
+'''
+
+
+
+
+
+
+'''
+Write a range that counts down from 30 to 0
+'''
+
+
+
+'''
+Exercise
+Rewrite the for loop we made last class that goes through a list and prints each item in the list, along with its index. (Hint: you can use a range, and you won't need a separate counter variable.)
+
+Example:
+planets = ["mercury", "venus", "earth", "mars"]
+0: mercury, 1: venus, 2: earth, 3: mars
+'''
+
+planets = ["mercury", "venus", "earth", "mars"]
+output = ''
+
+
+
+
+
+''' Exercise
+You have a list of employees, and a list of job titles. Assume the lists are the same length and in the same order.
+Use one for loop to go through both lists and print the job title of each employee.
+For example, if these are your lists:
+employees = ['Bob', 'Cynthia', 'Abdul']
+job_titles = ['accountant', 'engineer', 'recruiter']
+This should be your output:
+Bob's job title is accountant.
+Cynthia's job title is engineer.
+Abdul's job title is recruiter.
+
+'''
+
+
+employees = ['Bob', 'Cynthia', 'Abdul']
+job_titles = ['accountant', 'engineer', 'recruiter']
+
+
+
+'''
+Write some code that creates a range based on what the user enters. 
+Challenge: you can make a range with 1, 2, or 3 numbers. How would you allow the user to pick any of these options?
+'''
