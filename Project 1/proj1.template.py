9ef53c9a0b271cfbb0129dff54bff5d1e12f09c4
Chris Kayembe <chris.kayembe@talent.cancode.org>
2024-07-18 00:30:43 -0400

    Step 2 Working

--- Project 1/proj1.template.py
+++ Project 1/proj1.template.py
@@ -1,30 +1,31 @@
 
 ''' Print statement outlining rules for your application'''
-msg_1 = input('hello user, please read the username and password requirements to log in. Press OK to continue:')
-msg_2 = input('The username must start with a lowercase letter, It must only contain letters, numbers and underscores, It must not have any spaces. Type OK to continue:')
-msg_3 = input('The password must contain at least one digit, contain at least one special character, and not have any spaces. Type OK to continue:')
+msg_1 = ('hello user, To sign up enter a username and password')
+msg_2 = ('username must start with a lowercase letter, must only contain letters, numbers and underscores, and not have any spaces.')
+msg_3 = ('The password must contain at least one digit, contain at least one special character, and not have any spaces.')
 
-print(msg_1, msg_2,msg_3)
+print(msg_1) 
+print(msg_2)
+print(msg_3)
 
 
 ''' Initialize your variables, declaring variables 
 
 We will need 2 variables to capture the username and password. Another 2 variables to use as the system username and password to authenticate against when we register
 '''
-system_username , system_password = 'chris99','mango_man'
-username , password = '',''
+system_username = 'chris99'
+system_password = 'mango_man'
+
+username = ''
+password = ''
+
+#print(username , password , system_username , system_password)
 
 
 
 ''' A List to handle error messages '''
-errors = ['Please check username format', 'please check password format']
+error = ['Invalid Username', 'Invalid Password']
 
-username_format_error = errors[0]
-
-username = 56
-
-if username != 55:
-    print(username_format_error)
 
 
 ''' Start your while loop '''
